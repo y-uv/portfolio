b8ed57c509f2e2eaaa1068559835f54237e9322b
Yuval Smith <yuvalsmith1@gmail.com>
2023-10-19 14:37:29 -0400

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
 import SoundCloudCard from "./SoundCloudCard";
+import Header from "./Header";
 
 function App() {
-  return <SoundCloudCard />;
+  return (
+    <>
+      <Header />
+      <SoundCloudCard />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+  const linkedInUrl = "https://www.linkedin.com/in/yuval-smith-b92114241/";
+
+  return (
+    <header className="bg-black text-white py-8 text-center">
+      <div className="container mx-auto">
+        <a
+          href={linkedInUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-4xl font-bold transition duration-300 ease-in-out hover:text-amber-200 active:text-amber-500"
+        >
+          Yuval Smith
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
