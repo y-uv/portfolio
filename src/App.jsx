074e6bbe236106ac78413199fd3a3884f5becf79
Yuval Smith <yuvalsmith1@gmail.com>
2023-10-19 11:35:36 -0400

    header purple

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Yuval Smith</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p></p>
-      </div>
+      <h1 className="text-xl font-bold text-purple-300">Yuval Smith</h1>
     </>
   );
 }
