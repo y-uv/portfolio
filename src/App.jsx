5fb133b111fa4e15104b86d8bd3fd04af4fd118d
Yuval Smith <yuvalsmith1@gmail.com>
2023-10-19 11:59:02 -0400

    tailwind prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import SideBar from "./SideBar";
 
 function App() {
   return (
-    <>
-      <h1 className="text-xl font-bold text-purple-300">Yuval Smith</h1>
-    </>
+    <div className="flex">
+      <SideBar />
+    </div>
   );
 }
 
