d9687f212b7ccffbaccdb9980d911e12999d6835
Yuval Smith <yuvalsmith1@gmail.com>
2023-10-19 21:44:07 -0400

    spaced header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,19 +1,30 @@
-import React from "react";
-
 const Header = () => {
   const linkedInUrl = "https://www.linkedin.com/in/yuval-smith-b92114241/";
 
   return (
     <header className="bg-black text-white py-4 text-center">
-      <div className="container mx-auto text-center lg:text-left">
+      <div className="container mx-auto flex flex-row justify-center items-center lg:flex-row lg:justify-between lg:items-center">
         <a
           href={linkedInUrl}
           target="_blank"
           rel="noopener noreferrer"
-          className="text-2xl font-bold transition duration-400 ease-in-out hover:text-yuvhover active:text-yuvactive inline-block mx-auto lg:mx-20"
+          className="tracking-widest text-2xl font-bold transition duration-400 ease-in-out hover:text-yuvhover active:text-yuvactive mx-auto lg:mx-20 mb-2 lg:mb-0"
         >
           Yuval Smith
         </a>
+        <div className="flex justify-between">
+          <p className="tracking-tighter text-1xl font-bold transition duration-400 ease-in-out hover:text-yuvhover active:text-yuvactive mx-4">
+            About me
+          </p>
+
+          <p className="tracking-tighter text-1xl font-bold transition duration-400 ease-in-out hover:text-yuvhover active:text-yuvactive mx-4">
+            Testimonials
+          </p>
+
+          <p className="tracking-tighter text-1xl font-bold transition duration-400 ease-in-out hover:text-yuvhover active:text-yuvactive mx-4">
+            Projects
+          </p>
+        </div>
       </div>
     </header>
   );
